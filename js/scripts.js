667adb4063ce70bc0dd54647e7c2dd58e59f887d
Tam Nguyen <mr.tamron@gmail.com>
2016-07-15 22:15:54 -0700

    add user-interface pizza logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,61 @@
-//business_logic
-function Pizza(firstName, lastName, size, toppings, quantity, totalPrice) {
+// business logic
+function Pizza(firstName, lastName, size, toppings, quantity) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.size = size;
   this.toppings = toppings;
   this.quantity = quantity;
-  this.totalPrice = totalPrice;
 }
-
-//customer information
+// customer information
 Pizza.prototype.customerInfo = function () {
   return this.firstName + " " + this.lastName;
 }
-
-//calculating price of pizza
+// calculating price of pizza
 Pizza.prototype.pizzaCost = function () {
-  var totalToppings = this.toppings.length * .75;
-  var onePizza = totalToppings + this.size;
-  var totalPrice = onePizza * this.quantity;
-  this.totalPrice = totalPrice;
-  return this.totalPrice;
+  var size;
+  if (this.size === "Small") {
+    size = 10;
+  } else if (this.size === "Medium") {
+    size = 12;
+  } else if (this.size === "Large") {
+    size = 14;
+  } else if (this.size === "X-Large") {
+    size = 16;
+  }
+  var toppings = this.toppings.length * .75;
+  var onePizza = toppings + size;
+  var total = onePizza * this.quantity;
+  this.totalPrice = total;
+  return "$" + this.totalPrice;
 }
 
-//total pizza price message
-Pizza.prototype.messagePrice = function() {
-  return "Your total price is: " + this.totalPrice;
-}
-
-//user_interface_logic
+// user interface logic
 $(document).ready(function() {
-
-
+  $("form#new-pizza-customer").submit(function(event) {
+    event.preventDefault();
+    // grab values from form
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedSize = $("select#size").val();
+    var inputtedToppings = [];
+    $(":checkbox:checked").each(function(topping){
+      inputtedToppings[topping] = $(this).val();
+    });
+    var inputtedQuantity = $("select#quantity").val();
+    // instance of pizza constructor
+    var pizzaOrder = new Pizza(inputtedFirstName, inputtedLastName, inputtedSize, inputtedToppings, inputtedQuantity);
+    // order info
+    $("#pizza-info").show();
+    $(".full_name").text(pizzaOrder.customerInfo());
+    $(".pizza_size").text(pizzaOrder.size);
+    $(".pizza_toppings").text(pizzaOrder.toppings);
+    $(".pizza_quantity").text(pizzaOrder.quantity);
+    $(".total_price").text(pizzaOrder.pizzaCost());
+    // clear form after submit
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("select#size").val("");
+    $(":checkbox:checked").prop("checked", false);
+    $("select#quantity").val("");
+  });
 });
